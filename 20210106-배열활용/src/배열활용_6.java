27682ddd215a513a12d144bf9937a163f1151d87
SIST <SIST@DESKTOP-J6TRPCU>
2021-01-06 12:51:47 +0900

    활용

---
+++ 20210106-배열활용/src/배열활용_6.java
@@ -0,0 +1,27 @@
+// 문자열 => String => char[] 
+/*
+ *   String : 데이터형 , 클래스 (문자열을 제어 할 수 있는 기능=메소드)
+ *   문자열에서 문자한개를 가지고 올때   => charAt(0) 
+ *      ABCDEFG
+ *      0123456
+ *   문자열의 갯수 확인 length()
+ *   문자열 비교 equals => Login (id,pwd)
+ *   문자열 포함된 단어 contains() 
+ *   =============
+ *   시작하는 문자열 startsWith()
+ *   끝나는 문자열  endsWith()
+ *   ============= 서제스트 (검색)
+ *   문자열 자르기 substring()  ==> A.java  02)111-1111
+ *   문자열 => 대문자 , 소문자 
+ *           =====   =====
+ *           toUpperCase() , toLowerCase()
+ *   
+ */
+public class 배열활용_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
