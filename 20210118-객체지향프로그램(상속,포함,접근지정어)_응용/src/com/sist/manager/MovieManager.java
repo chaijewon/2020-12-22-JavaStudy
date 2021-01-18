ddd381f33babf854a3d14153ff81cb9d389a5220
SIST <SIST@DESKTOP-J6TRPCU>
2021-01-18 16:53:57 +0900

    1111

---
+++ 20210118-객체지향프로그램(상속,포함,접근지정어)_응용/src/com/sist/manager/MovieManager.java
@@ -0,0 +1,93 @@
+package com.sist.manager;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class MovieManager {
+   public MovieVO[] movieAllData()
+   {
+	   MovieVO[] movie=new MovieVO[7];// 데이터를 7개 모아서 main으로 전송
+	   try
+   	{
+   		FileReader fr=new FileReader("c:\\javaDev\\movie.json"); // movie.json파일안에 7개의 영화데이터
+   		String data="";
+   		int i=0;// 문자 번호 => 'A' => 65
+   		while((i=fr.read())!=-1) // -1 파일끝  read()=>한글자씩 읽어온다 
+   		{
+   			data+=String.valueOf((char)i); // 문자열 결합 
+   			//               문자 번호를 문자로 변환 => String에 저장
+   			// 데이터가 많은 경우에는 String => StringBuffer
+   			/*
+   			 *   + => 산술연산자 , 문자열 결합 
+   			 */
+   		}
+   		fr.close();// 파일 닫기 
+   		System.out.println(data); // 전체가 파일에 묶였는지 확인 
+   		
+   		// 파싱 (파일에서 관련된 데이터 읽기)
+   		/*
+   		 *    {} => JSONObject
+   		 *    [] => JSONArray
+   		 *    
+   		 *    {"datas":[{"actor":"가엘 가르시아 베르날 , 앤서니 곤잘레스 , 벤자민 브랫",
+              "director":"리 언크리치",
+              "grade":"전체",
+				"title":"코코",
+				"poster":"http:\/\/img.cgv.co.kr\/Movie\/Thumbnail\/Poster\/000080\/80397\/80397_185.jpg",
+				"mno":1,
+				"story"
+				
+				movie[i].setTitle("코코");// 데이터를 한개씩 읽어 온다 
+   			    movie[i].setActor("가엘 가르시아 베르날 , 앤서니 곤잘레스 , 벤자민 브랫");
+   		 *    
+   		 */
+   		JSONParser jp=new JSONParser();
+   		JSONObject root=(JSONObject)jp.parse(data);
+   		System.out.println(root.toJSONString());// 문자열 변환 
+   		//{datas
+   		JSONArray arr=(JSONArray)root.get("datas");
+   		System.out.println(arr.toJSONString());
+   		//[
+   		//System.out.println(arr.size());
+   		for(i=0;i<7;i++)
+   		{
+   			movie[i]=new MovieVO();// 객체 배열 
+   			/*
+   			 *    ======================  ==> 7개를 만들어서 데이터 수집 
+   			 *           mno
+   			 *    ======================
+   			 *          title
+   			 *    ======================
+   			 *          director
+   			 *    ======================
+   			 *          actor
+   			 *    ======================
+   			 *          poster
+   			 *    ======================
+   			 *          grade
+   			 *    ======================
+   			 *          story
+   			 *    ======================
+   			 */
+   			System.out.println("movie["+i+"]:"+movie[i]);
+   			JSONObject obj=(JSONObject)arr.get(i);
+   			System.out.println(obj.toJSONString());
+   			// JSON => 데이터 (int=>long)
+   			//int mno=(int)obj.get("mno");
+   			//movie[i].setMno(mno);
+   			// Integer.parseInt => String을 int변환
+   			movie[i].setTitle((String)obj.get("title"));// 데이터를 한개씩 읽어 온다 
+   			movie[i].setActor((String)obj.get("actor"));
+   			movie[i].setPoster((String)obj.get("poster"));
+   			movie[i].setDirector((String)obj.get("director"));
+   			movie[i].setGrade((String)obj.get("grade"));
+   			movie[i].setStory((String)obj.get("story"));
+   			// 변수의 초기값을 설정 
+   		}
+   	}catch(Exception ex){}
+	   return movie;
+   }
+}
