b1e9e98404c465a7010235fef4bc15050a34c0d5
SIST <SIST@DESKTOP-J6TRPCU>
2021-01-22 08:32:08 +0900

    1111

--- 20210120-인터페이스(내부클래스)/src/com/sist/main2/MainClass.java
+++ 20210120-인터페이스(내부클래스)/src/com/sist/main2/MainClass.java
@@ -13,13 +13,13 @@ class Oracle implements DataBase
 	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("오라클 연결");
 	}
 
 	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("오라클 닫기");
 	}
 	
 }
@@ -29,13 +29,13 @@ class MsSQL implements DataBase
 	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("MsSQL 연결");
 	}
 
 	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("MsSQL 닫기");
 	}
 	
 }
@@ -45,13 +45,13 @@ class MySQL implements DataBase
 	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("MySQL 연결");
 	}
 
 	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("MySQL 닫기");
 	}
 	
 }
@@ -61,21 +61,25 @@ class DB2 implements DataBase
 	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("DB2 연결");
 	}
 
 	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("DB2 닫기");
 	}
 	
 }
+// interface는 주로 하는 역할 : 여러개를 클래스를 모아서 관리하는 프로그램 (Web에서 주로 사용:스프링)
+// JSP / Asp.net / PHP
+// Web => 데이터베이스 연결 
+// Java:인터페이스    C#: 클래스
 class Container
 {
 	public DataBase dbSelect(String type)
 	{
-		DataBase db=null;
+		DataBase db=null;// Map
 		if(type.equals("Oracle"))
 			db=new Oracle();
 		else if(type.equals("MsSQL"))
@@ -88,11 +92,26 @@ class Container
 		return db;
 	}
 }
+/*
+ *    인터페이스 : 리모콘 
+ *    ========
+ *      자바가 리모콘을 제작하는 업체 => 부도 => 브라우저(네스케이프):웹중심 
+ *      
+ *      HTML5 => MS제외 (Google,FF,오페라) => HTML(추상적) , HTML5(알기쉽게:시멘텍)
+ *      <object>  <video>
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        
+        Container c=new Container();
+        DataBase db=c.dbSelect("Oracle");
+        db.connect();
+        db.disconnect();
+        System.out.println("===================");
+        db=c.dbSelect("DB2");
+        db.connect();
+        db.disconnect();
 	}
 
 }
